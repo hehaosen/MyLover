f376f2898cc17146f88972bd8c41132ccbafefcf
hehaosen <307052084@qq.com>
2016-07-08 10:20:31 +0800

    ＋Login board

--- login.js
+++ login.js
@@ -20,7 +20,14 @@ export default class Login extends Component {
                 <Image style={styles.bg}
                        source={require('./src/images/login/bg.png')}>
                     <Text style={styles.title}>爱+</Text>
+                    <View style={styles.inputBoard}>
+                        <View style={styles.userName}>
+                            <Image style={styles.inputMail}
+                                   source={require('./src/images/login/mail.png')}/>
+                        </View>
+                    </View>
                 </Image>
+
             </View>
         );
     }
@@ -36,12 +43,27 @@ const styles = StyleSheet.create({
         marginTop: PixelRatio.getPixelSizeForLayoutSize(150/4),
         backgroundColor: 'rgba(0, 0, 0, 0.0)',
         color: '#FFFFFF'
-
+    },
+    inputBoard: {
+        width: PixelRatio.getPixelSizeForLayoutSize(611/4),
+        height: PixelRatio.getPixelSizeForLayoutSize(273/4),
+        marginTop: PixelRatio.getPixelSizeForLayoutSize(100/4),
+        backgroundColor: '#666666'
     },
     bg: {
         resizeMode: Image.resizeMode.cover,
         width: PixelRatio.getPixelSizeForLayoutSize(650/4),
-        height: PixelRatio.getPixelSizeForLayoutSize(1320/4)
+        height: PixelRatio.getPixelSizeForLayoutSize(1320/4),
+        alignItems: 'center'
+    },
+    inputMail: {
+        width: PixelRatio.getPixelSizeForLayoutSize(26/4),
+        height: PixelRatio.getPixelSizeForLayoutSize(20/4)
+    },
+    userName: {
+        height: PixelRatio.getPixelSizeForLayoutSize(92/4),
+        backgroundColor: '#004672',
+        margin: PixelRatio.getPixelSizeForLayoutSize(40/4)
     }
 });
 
