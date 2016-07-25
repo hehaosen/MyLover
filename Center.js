7b772d65c2675b1b6e65eaeddb1306a12bea8e78
hehaosen <307052084@qq.com>
2016-07-25 10:17:45 +0800

    +user center header background img

--- Center.js
+++ Center.js
@@ -31,8 +31,10 @@ export default class Center extends Component {
     }
     render() {
         return (
-            <View>
-                <View style={styles.header}>
+            <View style={styles.main}>
+                <Image style={styles.headerBg}
+                       source={require('./src/images/center/header-bg.png')}/>
+                    <View style={styles.header}>
                     <TouchableHighlight style={styles.otherLogin}
                                         underlayColor='#99d9f4'
                                         onPress={this.goBack.bind(this)}
@@ -41,7 +43,8 @@ export default class Center extends Component {
                                source={require('./src/images/public/back.png')}/>
                     </TouchableHighlight>
                     <Text style={styles.footMark}>我的足迹</Text>
-                </View>
+                    </View>
+
                 <View>
 
                 </View>
@@ -50,18 +53,29 @@ export default class Center extends Component {
     }
 }
 const styles = StyleSheet.create({
+    main: {
+        position:'relative'
+    },
     goBack: {
         width: PixelRatio.getPixelSizeForLayoutSize(24/4),
         height: PixelRatio.getPixelSizeForLayoutSize(41/4),
-        marginLeft: PixelRatio.getPixelSizeForLayoutSize(35/4)
+        left: PixelRatio.getPixelSizeForLayoutSize(35/4)
     },
     header: {
         flexDirection: 'row',
         marginTop: 25,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        position: 'absolute',
+        width:PixelRatio.getPixelSizeForLayoutSize(750/4)
     },
     footMark: {
-        marginRight: PixelRatio.getPixelSizeForLayoutSize(35/4)
+        left: PixelRatio.getPixelSizeForLayoutSize(500/4),
+        color: '#0079ff'
+    },
+    headerBg: {
+        width: PixelRatio.getPixelSizeForLayoutSize(750/4),
+        height: PixelRatio.getPixelSizeForLayoutSize(420/4),
+        resizeMode: Image.resizeMode.cover,
+        position: 'absolute',
     }
 });
