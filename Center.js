223277b20090eadaebf9e2030e87d0bdf109dbef
hehaosen <307052084@qq.com>
2016-07-26 12:14:16 +0800

    dd

--- Center.js
+++ Center.js
@@ -34,7 +34,7 @@ export default class Center extends Component {
             <View style={styles.main}>
                 <Image style={styles.headerBg}
                        source={require('./src/images/center/header-bg.png')}/>
-                    <View style={styles.header}>
+                <View style={styles.header}>
                     <TouchableHighlight style={styles.otherLogin}
                                         underlayColor='#99d9f4'
                                         onPress={this.goBack.bind(this)}
@@ -43,8 +43,12 @@ export default class Center extends Component {
                                source={require('./src/images/public/back.png')}/>
                     </TouchableHighlight>
                     <Text style={styles.footMark}>我的足迹</Text>
-                    </View>
+                </View>
 
+                <View style={styles.portrait}>
+                    <Image style={styles.goBack}
+                           source={require('./src/images/public/back.png')}/>
+                </View>
                 <View>
 
                 </View>
@@ -54,7 +58,9 @@ export default class Center extends Component {
 }
 const styles = StyleSheet.create({
     main: {
-        position:'relative'
+        position:'relative',
+        alignItems: 'center',
+
     },
     goBack: {
         width: PixelRatio.getPixelSizeForLayoutSize(24/4),
@@ -64,7 +70,6 @@ const styles = StyleSheet.create({
     header: {
         flexDirection: 'row',
         marginTop: 25,
-        alignItems: 'center',
         position: 'absolute',
         width:PixelRatio.getPixelSizeForLayoutSize(750/4)
     },
@@ -75,7 +80,15 @@ const styles = StyleSheet.create({
     headerBg: {
         width: PixelRatio.getPixelSizeForLayoutSize(750/4),
         height: PixelRatio.getPixelSizeForLayoutSize(420/4),
+        left: -PixelRatio.getPixelSizeForLayoutSize(420/20),
         resizeMode: Image.resizeMode.cover,
-        position: 'absolute',
+        position: 'absolute'
+    },
+    portrait:{
+        width: PixelRatio.getPixelSizeForLayoutSize(245/4),
+        height: PixelRatio.getPixelSizeForLayoutSize(245/4),
+        borderColor: '#FFFFFF',
+        borderWidth: 3,
+        borderStyle: 'solid',
     }
 });
